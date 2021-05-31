794fc8ecbd963a56fb8fdaf0a3afba159200d8d9
Guilherme Tinem <guizstinem@gmail.com>
2021-05-31 11:56:34 -0300

    fix: Alterado o nome de "Register" para "Records"

--- database/migrations/2021_05_31_141550_create_records_table.php
+++ database/migrations/2021_05_31_141550_create_records_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegisterTable extends Migration
+class CreateRecordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRegisterTable extends Migration
      */
     public function up()
     {
-        Schema::create('register', function (Blueprint $table) {
+        Schema::create('records', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('name', 100);
             $table->string('email', 80);
@@ -38,6 +38,6 @@ class CreateRegisterTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('register');
+        Schema::dropIfExists('records');
     }
 }
